03d1f0f60f89f9bc79ec248cd2e0cfb922825565
Adler <nkj20932@hotmail.com>
2015-04-01 21:42:58 +0800

    refactor i18n

--- lib/power-build/constructor.rb
+++ lib/power-build/constructor.rb
@@ -6,7 +6,6 @@ require 'json'
 
 module PowerBuild
   class Constructor
-
     def initialize(config=nil)
       @assets_base = "../../assets"
       variables_set if config
@@ -101,7 +100,7 @@ module PowerBuild
                                   resource_prefix: "",
                                   copyright_display: @config["Copyright Display"]
                                  )
-      i18n(@config["language"]).each {|key, value| @variables.send("#{key.to_s}=".to_sym, value) }
+      I18n.load(@config["language"]).each {|key, value| @variables.send("#{key.to_s}=".to_sym, value) }
     end
 
     def copy_assets_dir(dir_name)
@@ -194,31 +193,6 @@ module PowerBuild
       erb = ERB.new(content).result(@variables.instance_eval{binding})
       File.open("#{@dir}/#{@variables.current_title}.html", "w") {|file| file.write(erb)}
       puts "Created: ".green + "#{@dir}/#{@variables.current_title}.html"
-    end
-
-    def i18n(setting)
-      case setting
-      when "zh-tw"
-        {
-          i_header: "總覽",
-          i_category: "類別",
-          i_copyright: "版權所有",
-          i_download: "下載",
-          i_home: "首頁",
-          i_uncategorized: "綜合",
-          i_copy: "複製圖片連結"
-        }
-      when "en"
-        {
-          i_header: "Overview",
-          i_category: "Categories",
-          i_copyright: "Copyright",
-          i_download: "Download",
-          i_home: "Home",
-          i_uncategorized: "Uncategorized",
-          i_copy: "Copy"
-        }
-      end
     end
   end
 end
--- lib/power-build/helper.rb
+++ lib/power-build/helper.rb
@@ -1,3 +1,30 @@
+class I18n
+  def self.load(setting) 
+    case setting
+    when "zh-tw"
+      {
+        i_header: "總覽",
+        i_category: "類別",
+        i_copyright: "版權所有",
+        i_download: "下載",
+        i_home: "首頁",
+        i_uncategorized: "綜合",
+        i_copy: "複製圖片連結"
+      }
+    when "en"
+      {
+        i_header: "Overview",
+        i_category: "Categories",
+        i_copyright: "Copyright",
+        i_download: "Download",
+        i_home: "Home",
+        i_uncategorized: "Uncategorized",
+        i_copy: "Copy"
+      }
+    end
+  end
+end
+
 class Array
   def delete_folder_extra
     array = self
