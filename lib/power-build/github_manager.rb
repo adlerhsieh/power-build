e663129206983cc71003fb95fb59b4ad63d95f87
Adler <nkj20932@hotmail.com>
2015-04-01 11:06:50 +0800

    comment out git function

--- lib/power-build/github_manager.rb
+++ lib/power-build/github_manager.rb
@@ -7,14 +7,37 @@ module PowerBuild
       
     end
 
-    def checkout_branch
-      command = %x[git branch]
-      # if command.include? "gh-pages"
-      #   %x[git checkout gh-pages]
-      # else
-      #   %x[git checkout -b gh-pages]
-      # end
-      puts command
-    end
+    # def checkout_branch
+    #   command = %x[git branch]
+    #   if command.include? "gh-pages"
+    #     command = %x[git checkout gh-pages]
+    #   else
+    #     command = %x[git checkout -b gh-pages]
+    #   end
+    #   add_and_commit
+    # end
+    #
+    # def add_and_commit
+    #   status = %x[git status]
+    #   if status.include? "assets"
+    #     %x[git add assets] 
+    #     @commit = true
+    #   end
+    #   if status.include? "index.html"
+    #     %x[git add index.html] 
+    #     @commit = true
+    #   end
+    #   if status.include? "power-build.config"
+    #     %x[git add power-build.config] 
+    #     @commit = true
+    #   end
+    #   if @commit
+    #     %x[git commit -m "regenerate site"]
+    #   end
+    # end
+    #
+    # def push
+    #   %x[git push]
+    # end
   end
 end
